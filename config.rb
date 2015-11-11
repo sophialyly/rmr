b6e099f5d6d626b79f1a046e5ea541594813b529
anusorn <scada.josh@gmail.com>
2015-11-11 20:23:52 +0700

    2015-11-11 wlma-admin

--- config.rb
+++ config.rb
@@ -57,9 +57,62 @@ set :images_dir, 'images'
 
 set :helper_dir, 'helper'
 
+set :partials_desktop_Common_dir, 'src/desktop/Common'
+set :partials_desktop_Admin_blank_dir, 'src/desktop/Admin/blank'
+set :partials_desktop_Admin_index_dir, 'src/desktop/Admin/index'
+
 
 # Build-specific configuration
 configure :build do
+  # Any files you want to ignore:
+  ignore '/javascripts/javascript-desktop-admin/*'
+  ignore '/javascripts/javascript-desktop-admin.js.erb'
+  ignore '/javascripts/javascript-flaty_theme/*'
+  ignore '/javascripts/javascript-flaty_theme.js.erb'
+  ignore '/javascripts/javascript-flaty_common/*'
+  ignore '/javascripts/javascript-flaty_login/*'
+  ignore '/javascripts/javascript-flaty_blank/*'
+  ignore '/javascripts/javascript-flaty_index/*'
+  ignore '/javascripts/javascript-training-section1/*'
+  ignore '/javascripts/javascript-training-section1.js.erb'
+  ignore '/javascripts/javascript-training-section2/*'
+  ignore '/javascripts/javascript-training-section2.js.erb'
+  ignore '/javascripts/javascript-training-section3/*'
+  ignore '/javascripts/javascript-training-section3.js.erb'
+  ignore '/javascripts/javascript-training-section4/*'
+  ignore '/javascripts/javascript-training-section4.js.erb'
+  ignore '/javascripts/all.js'
+
+  ignore '/stylesheets/stylesheet-desktop-admin/*'
+  ignore '/stylesheets/stylesheet-desktop-admin.css.erb'
+  ignore '/stylesheets/stylesheet-flaty_theme/*'
+  ignore '/stylesheets/stylesheet-flaty_common/*'
+  ignore '/stylesheets/stylesheet-flaty_login/*'
+  ignore '/stylesheets/stylesheet-flaty_blank/*'
+  ignore '/stylesheets/stylesheet-flaty_index/*'
+  ignore '/stylesheets/stylesheet-training-section1/*'
+  ignore '/stylesheets/stylesheet-training-section1.css.erb'
+  ignore '/stylesheets/stylesheet-training-section2/*'
+  ignore '/stylesheets/stylesheet-training-section2.css.erb'
+  ignore '/stylesheets/stylesheet-training-section3/*'
+  ignore '/stylesheets/stylesheet-training-section3.scss'
+  ignore '/stylesheets/stylesheet-training-section4/*'
+  ignore '/stylesheets/stylesheet-training-section4.scss'
+  ignore '/stylesheets/stylesheet-flaty_theme.css.erb'
+  ignore '/stylesheets/all.css'
+  ignore '/stylesheets/hello.scss'
+  ignore '/stylesheets/normalize.css'
+
+  ignore '/src/desktop/training-section1/*'
+  ignore '/src/desktop/training-section2/*'
+  ignore '/src/desktop/training-section3/*'
+  ignore '/src/desktop/training-section4/*'
+  ignore '/src/desktop/Admin_2015-11-10/*'
+
+  ignore '/images/images-training-section2/*'
+  ignore '/images/images-training-section3/*'
+  ignore '/images/images-training-section4/*'
+
   # For example, change the Compass output style for deployment
   # activate :minify_css
   activate :php
