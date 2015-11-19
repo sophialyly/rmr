72777a506a608331dd3cc1816c47ed3b6b34d71c
anusorn <scada.josh@gmail.com>
2015-11-19 19:59:26 +0700

    2015-11-19 rmr v2

--- config.rb
+++ config.rb
@@ -63,7 +63,7 @@ set :partials_desktop_Home_index_dir, 'src/desktop/Home'
 
 set :partials_desktop_home_rmr_reportCost, 'src/desktop/Home/RMR/ReportCost'
 set :partials_desktop_home_rmr_reportDeviceRepair, 'src/desktop/Home/RMR/ReportDeviceRepair'
-set :partials_desktop_Admin_rmr_reportRTU, 'src/desktop/Admin/rmr_reportRTU'
+set :partials_desktop_home_rmr_reportRTU, 'src/desktop/Home/RMR/reportRTU'
 set :partials_desktop_home_rmr_searchRTU, 'src/desktop/Home/RMR/searchRTU'
 
 set :partials_desktop_Admin_administrator_member, 'src/desktop/Admin/administrator_member'
