46ed7a82b8ca91707e053c4e798e8b38c203c1e0
anusorn <scada.josh@gmail.com>
2016-04-24 08:42:40 +0700

    2016-04-24 rmr

---
+++ build/src/unit-test/Leaflet/creating-interactive-choropleth-map.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>creating-interactive-choropleth-map</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        
+        <link href="../../../stylesheets/stylesheet-unit-test-leaflet-01.css" rel="stylesheet" type="text/css" />
+        
+        
+
+    </head>
+    <body>
+
+        <div id="map"></div>
+        
+        
+        <script src="../../../javascripts/javascript-unit-test-leaflet-01.js" type="text/javascript"></script>
+        <script src="http://maps.google.com/maps/api/js?v=3.2&sensor=false"></script>
+        <script src="http://matchingnotes.com/javascripts/leaflet-google.js"></script>
+
+    </body>
+</html>
